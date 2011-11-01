b7e11940459d529b82ae939eda1a44e24bec5297
Ionel Maries Cristian <contact@ionelmc.ro>
2011-11-01 17:56:58 +0200

    Added filters for details and prettify for timestamp/second values.

--- src/redisboard/admin.py
+++ src/redisboard/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
+from django.conf import settings
 from .models import RedisServer
+
+import re
+from datetime import datetime, timedelta
+
+REDISBOARD_DETAIL_FILTERS = [re.compile(name) for name in getattr(settings, 'REDISBOARD_DETAIL_FILTERS', (
+    'aof_enabled', 'bgrewriteaof_in_progress', 'bgsave_in_progress',
+    'changes_since_last_save', 'db.*', 'db1', 'last_save_time',
+    'multiplexing_api', 'total_commands_processed',
+    'total_connections_received', 'uptime_in_days', 'uptime_in_seconds',
+    'vm_enabled'
+))]
+REDISBOARD_DETAIL_TIMESTAMP_KEYS = getattr(settings, 'REDISBOARD_DETAIL_TIMESTAMP_KEYS', (
+    'last_save_time',
+))
+REDISBOARD_DETAIL_SECONDS_KEYS = getattr(settings, 'REDISBOARD_DETAIL_SECONDS_KEYS', (
+    'uptime_in_seconds',
+))
+
+
+def prettify(key, value):
+    if key in REDISBOARD_DETAIL_SECONDS_KEYS:
+        return key, timedelta(seconds=value)
+    elif key in REDISBOARD_DETAIL_TIMESTAMP_KEYS:
+        return key, datetime.fromtimestamp(value)
+    else:
+        return key, value
 
 class RedisServerAdmin(admin.ModelAdmin):
     list_display = (
@@ -16,8 +43,9 @@ class RedisServerAdmin(admin.ModelAdmin):
 
     def details(self, obj):
         return "<table>%s</table>" % ''.join(
-            "<tr><td>%s</td><td>%s</td></tr>" % i
-                for i in sorted(obj.stats['details'].items(), key=lambda (k,v): k)
+            "<tr><td>%s</td><td>%s</td></tr>" % prettify(k, v)
+                for k, v in sorted(obj.stats['details'].items(), key=lambda (k,v): k)
+                if any(name.match(k) for name in REDISBOARD_DETAIL_FILTERS)
         )
 
     details.allow_tags = True
