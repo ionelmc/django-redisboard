8b79ebc6f2134d435ed6cb461a17992c3033f5f1
Ionel Maries Cristian <contact@ionelmc.ro>
2011-11-08 01:58:53 +0200

    Added key inspection.

--- src/redisboard/views.py
+++ src/redisboard/views.py
@@ -3,6 +3,8 @@ logger = getLogger(__name__)
 
 from django.shortcuts import render
 from django.utils.datastructures import SortedDict
+from django.conf import settings
+
 from redis.exceptions import ResponseError
 
 def safeint(value):
@@ -14,44 +16,84 @@ def safeint(value):
 def _fixup_pair((a, b)):
     return a, safeint(b)
 
-def _get_key_details(conn, db):
+LENGTH_GETTERS = {
+    'list': lambda conn, key: conn.llen(key),
+    'string': lambda conn, key: conn.strlen(key),
+    'set': lambda conn, key: conn.scard(key),
+    'zset': lambda conn, key: conn.zcount(key, '-inf', '+inf'),
+    'hash': lambda conn, key: conn.hlen(key),
+}
+
+def _get_key_info(conn, key):
+    try:
+        details = conn.execute_command('DEBUG', 'OBJECT', key)
+        obj_type = conn.type(key)
+        obj_length = LENGTH_GETTERS[obj_type](conn, key)
+        return {
+            'type': conn.type(key),
+            'name': key,
+            'details': dict(
+                _fixup_pair(i.split(':')) for i in details.split() if ':' in i
+            ),
+            'length': obj_length,
+            'ttl': conn.ttl(key),
+        }
+    except ResponseError, e:
+        logger.exception("Failed to get details for key %r", key)
+        return {
+            'type': "n/a",
+            'length': "n/a",
+            'name': key,
+            'details': {},
+            'error': e,
+            'ttl': "n/a",
+        }
+
+VALUE_GETTERS = {
+    'list': lambda conn, key, start=0, end=-1: enumerate(conn.lrange(key, start, end)),
+    'string': lambda conn, key, *args: [('string', conn.get(key))],
+    'set': lambda conn, key, *args: enumerate(conn.smembers(key)),
+    'zset': lambda conn, key, start=0, end=-1: enumerate(conn.zrange(key, start, end)),
+    'hash': lambda conn, key, *args: conn.hgetall(key).iteritems(),
+}
+
+def _get_key_details(conn, db, key):
+    conn.execute_command('SELECT', db)
+    details = _get_key_info(conn, key)
+    details['data'] = VALUE_GETTERS[details['type']](conn, key)
+    return details
+
+
+def _get_db_details(conn, db):
     conn.execute_command('SELECT', db)
     keys = conn.keys()
     key_details = {}
     for key in keys:
-        try:
-            details = conn.execute_command('DEBUG', 'OBJECT', key)
-            key_details[key] = {
-                'type': conn.type(key),
-                'details': dict(
-                    _fixup_pair(i.split(':')) for i in details.split() if ':' in i
-                ),
-                'ttl': conn.ttl(key),
-            }
-        except ResponseError, e:
-            logger.exception("Failed to get details for key %r", key)
-            key_details[key] = {
-                'type': "n/a",
-                'details': {},
-                'error': e,
-                'ttl': "n/a",
-            }
+        key_details[key] = _get_key_info(conn, key)
     return key_details
 
 
 def inspect(request, server):
     stats = server.stats
+    conn = server.connection
+    database_details = SortedDict()
+    key_details = None
+
     if stats['status'] == 'UP':
-        conn = server.connection
-        databases = sorted(name[2:] for name in conn.info() if name.startswith('db'))
-        database_details = SortedDict()
-        for db in databases:
-            database_details[db] = _get_key_details(conn, db)
-    else:
-        database_details = {}
+        if 'key' in request.GET:
+            key = request.GET['key']
+            db = request.GET.get('db', 0)
+            key_details = _get_key_details(conn, db, key)
+        else:
+            databases = sorted(name[2:] for name in conn.info() if name.startswith('db'))
+
+            for db in databases:
+                database_details[db] = _get_db_details(conn, db)
+
 
     return render(request, "redisboard/inspect.html", {
         'databases': database_details,
+        'key_details': key_details,
         'original': server,
         'stats': stats,
         'app_label': 'redisboard',
