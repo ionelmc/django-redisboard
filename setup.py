e636a64f5b5cd40f89b8b55071e998e7c38b1e5a
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-02-16 14:49:08 +0200

    Update setup.py.

--- setup.py
+++ setup.py
@@ -4,25 +4,25 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name = "django-redisboard",
-    version = "0.2.7",
-    url = 'https://github.com/ionelmc/django-redisboard',
-    download_url = '',
-    license = 'BSD',
-    description = "Brief redis monitoring in django admin",
-    long_description = file(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
-    author = 'Ionel Cristian Mărieș',
-    author_email = 'contact@ionelmc.ro',
-    packages = find_packages('src'),
-    package_dir = {'':'src'},
-    include_package_data = True,
-    install_requires = [
+    name="django-redisboard",
+    version="0.2.7",
+    url='https://github.com/ionelmc/django-redisboard',
+    download_url='',
+    license='BSD',
+    description="Brief redis monitoring in django admin",
+    long_description=file(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Ionel Cristian Mărieș',
+    author_email='contact@ionelmc.ro',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
         'redis',
         'Django>=1.3',
     ],
-    zip_safe = False,
-    classifiers = [
-        'Development Status :: 4 - Beta',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
