d2394f999ff7a99f1e90799c1f94f4a15313ad19
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-06-30 09:30:18 +0300

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='django-redisboard',
-    version='1.2.0',
+    version='1.2.1',
     license='BSD',
     description='Redis monitoring and inspection drop-in application using django admin.',
     long_description='%s\n%s' % (read('README.rst'), re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))),
