d82177992cc4317cdc1f694fccaeee196dd42aba
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-06-30 12:56:34 +0300

    Workaround for screwy 1.4 issue.

--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -10,8 +10,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+TEMPLATE_DIRS = [
+    os.path.join(sys.prefix, 'django', 'contrib', 'admin', 'templates')
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
