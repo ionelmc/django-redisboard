c26cc339f732a513e63ecabf427c25691e673992
jolks <jolks@outlook.com>
2015-10-09 23:47:36 +0900

    Handle Exception for Redis with no DEBUG OBJECT command. Allow key/value monitoring

--- src/redisboard/views.py
+++ src/redisboard/views.py
@@ -58,12 +58,12 @@ def _get_key_info(conn, key):
     except ResponseError as exc:
         logger.exception("Failed to get details for key %r", key)
         return {
-            'type': "n/a",
+            'type': conn.type(key),
             'length': "n/a",
             'name': key,
             'details': {},
             'error': str(exc),
-            'ttl': "n/a",
+            'ttl': conn.ttl(key),
         }
 
 VALUE_GETTERS = {
@@ -97,66 +97,75 @@ def _get_key_details(conn, db, key, page):
 
 
 def _get_db_summary(server, db):
-    server.connection.execute_command('SELECT', db)
-    pipe = server.connection.pipeline()
+    try:
+        server.connection.execute_command('SELECT', db)
+        pipe = server.connection.pipeline()
 
-    pipe.dbsize()
-    for i in range(server.sampling_threshold):
-        pipe.randomkey()
+        pipe.dbsize()
+        for i in range(server.sampling_threshold):
+            pipe.randomkey()
 
-    results = pipe.execute()
-    size = results.pop(0)
-    keys = sorted(set(results))
+        results = pipe.execute()
+        size = results.pop(0)
+        keys = sorted(set(results))
 
-    pipe = server.connection.pipeline()
-    for key in keys:
-        pipe.execute_command('DEBUG', 'OBJECT', key)
-        pipe.ttl(key)
+        pipe = server.connection.pipeline()
+        for key in keys:
+            pipe.execute_command('DEBUG', 'OBJECT', key)
+            pipe.ttl(key)
 
-    total_memory = 0
-    volatile_memory = 0
-    persistent_memory = 0
-    total_keys = 0
-    volatile_keys = 0
-    persistent_keys = 0
-    results = pipe.execute()
-    for key, details, ttl in zip(keys, results[::2], results[1::2]):
-        if not isinstance(details, dict):
-            details = dict(_fixup_pair(i.split(b':'))
-                           for i in details.split() if b':' in i)
-
-        length = details[b'serializedlength'] + len(key)
-
-        if ttl:
-            persistent_memory += length
-            persistent_keys += 1
-        else:
-            volatile_memory += length
-            volatile_keys += 1
-        total_memory += length
-        total_keys += 1
-
-    if total_keys:
-        total_memory = (total_memory / total_keys) * size
-    else:
         total_memory = 0
-
-    if persistent_keys:
-        persistent_memory = (persistent_memory / persistent_keys) * size
-    else:
-        persistent_memory = 0
-
-    if volatile_keys:
-        volatile_memory = (volatile_memory / volatile_keys) * size
-    else:
         volatile_memory = 0
+        persistent_memory = 0
+        total_keys = 0
+        volatile_keys = 0
+        persistent_keys = 0
+        results = pipe.execute()
+        for key, details, ttl in zip(keys, results[::2], results[1::2]):
+            if not isinstance(details, dict):
+                details = dict(_fixup_pair(i.split(b':'))
+                               for i in details.split() if b':' in i)
 
-    return dict(
-        size=size,
-        total_memory=total_memory,
-        volatile_memory=volatile_memory,
-        persistent_memory=persistent_memory,
-    )
+            length = details[b'serializedlength'] + len(key)
+
+            if ttl:
+                persistent_memory += length
+                persistent_keys += 1
+            else:
+                volatile_memory += length
+                volatile_keys += 1
+            total_memory += length
+            total_keys += 1
+
+        if total_keys:
+            total_memory = (total_memory / total_keys) * size
+        else:
+            total_memory = 0
+
+        if persistent_keys:
+            persistent_memory = (persistent_memory / persistent_keys) * size
+        else:
+            persistent_memory = 0
+
+        if volatile_keys:
+            volatile_memory = (volatile_memory / volatile_keys) * size
+        else:
+            volatile_memory = 0
+
+        return dict(
+            size=size,
+            total_memory=total_memory,
+            volatile_memory=volatile_memory,
+            persistent_memory=persistent_memory,
+        )
+    except ResponseError as exc:
+        logger.exception("Failed to get details for key %r", key)
+        return dict(
+            size=0,
+            total_memory=0,
+            volatile_memory=0,
+            persistent_memory=0,
+        )
 
 
 def _get_db_details(server, db):
