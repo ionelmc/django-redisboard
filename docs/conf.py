41437094a875069ebe8d268020d8efd6b522f065
Ionel Cristian Mărieș <contact@ionelmc.ro>
2020-07-17 18:28:56 +0300

    Update skel.

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import os
 
+
+import sphinx_py3doc_enhanced_theme
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -15,15 +16,10 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
 ]
-if os.getenv('SPELLCHECK'):
-    extensions += 'sphinxcontrib.spelling',
-    spelling_show_suggestions = True
-    spelling_lang = 'en_US'
-
 source_suffix = '.rst'
 master_doc = 'index'
 project = 'django-redisboard'
-year = '2011-2018'
+year = '2011-2020'
 author = 'Ionel Cristian Mărieș'
 copyright = '{0}, {1}'.format(year, author)
 version = release = '4.0.0'
@@ -34,7 +30,6 @@ extlinks = {
     'issue': ('https://github.com/ionelmc/django-redisboard/issues/%s', '#'),
     'pr': ('https://github.com/ionelmc/django-redisboard/pull/%s', 'PR #'),
 }
-import sphinx_py3doc_enhanced_theme
 html_theme = "sphinx_py3doc_enhanced_theme"
 html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
 html_theme_options = {
