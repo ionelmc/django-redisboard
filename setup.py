1cb90a3c11376f8daba98a91ca1ff5bd053489f9
Ionel Cristian Mărieș <contact@ionelmc.ro>
2022-10-10 15:17:20 +0300

    Bump version: 8.3.0 → 8.3.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*names, **kwargs):
 
 setup(
     name='django-redisboard',
-    version='8.3.0',
+    version='8.3.1',
     license='BSD-2-Clause',
     description='Redis monitoring and inspection drop-in application using django admin.',
     long_description='{}\n{}'.format(
--- src/redisboard/__init__.py
+++ src/redisboard/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '8.3.0'
+__version__ = '8.3.1'
 default_app_config = 'redisboard.apps.RedisboardApp'
